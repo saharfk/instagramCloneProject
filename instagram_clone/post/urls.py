9b67f666223c2ea71ca0c3698bef7bcf60152a50
saharfk78 <sahar.kashany@yahoo.com>
2021-05-17 00:25:57 +0430

    creat form for post

--- instagram_clone/post/urls.py
+++ instagram_clone/post/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from post.views import index
+from post.views import index, NewPost
 
 # , NewPost, PostDetails, tags, like, favorite
 
 urlpatterns = [
     path('', index, name='index'),
-    # path('newpost/', NewPost, name='newpost'),
+    path('newpost/', NewPost, name='newpost'),
     # path('<uuid:post_id>', PostDetails, name='postdetails'),
     # path('<uuid:post_id>/like', like, name='postlike'),
     # path('<uuid:post_id>/favorite', favorite, name='postfavorite'),
--- instagram_clone/post/views.py
+++ instagram_clone/post/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 
-from post.models import Stream, Post
+from post.models import Stream, Post, Tag
 # Tag, Likes, PostFileContent
-# from post.forms import NewPostForm
+from post.forms import NewPostForm
 # from stories.models import Story, StoryStream
 
 # from comment.models import Comment
@@ -42,3 +42,34 @@ def index(request):
 
     return HttpResponse(template.render(context, request))
 
+
+@login_required
+def NewPost(request):
+    user = request.user.id
+    tags_objs = []
+
+    if request.method == "POST":
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            picture = form.cleaned_data.get('picture')
+            caption = form.cleaned_data.get('caption')
+            tags_form = form.cleaned_data.get('tags')
+
+            tags_list = list(tags_form.split(','))
+            for tag in tags_list:
+                t, created = Tag.objects.get_or_create(title=tag)
+                tags_objs.append(t)
+
+            p, created = Post.objects.get_or_create(picture=picture, caption=caption, user_id=user)
+            p.tags.set(tags_objs)
+            p.save()
+            return redirect('index')
+
+    else:
+        form = NewPostForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'newpost.html', context)

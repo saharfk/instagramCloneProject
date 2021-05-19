0e1706a916e4fe55574332e470b8d36ed821e174
saharfk78 <sahar.kashany@yahoo.com>
2021-05-19 20:05:14 +0430

    activing comment

--- instagram_clone/post/views.py
+++ instagram_clone/post/views.py
@@ -6,6 +6,8 @@ from post.models import Stream, Post, Tag, Likes
 from post.forms import NewPostForm
 from django.contrib.auth.decorators import login_required
 from authy.models import Profile
+from comment.models import Comment
+from comment.forms import CommentForm
 
 
 # Create your views here.
@@ -65,19 +67,42 @@ def NewPost(request):
 @login_required
 def PostDetails(request, post_id):
     post = get_object_or_404(Post, id=post_id)
+    user = request.user
+    profile = Profile.objects.get(user=user)
     favorited = False
 
+    # comment
+    comments = Comment.objects.filter(post=post).order_by('date')
+
     if request.user.is_authenticated:
-        profile = Profile.objects.get(user=request.user)
+        profile = Profile.objects.get(user=user)
+        # For the color of the favorite button
 
         if profile.favorites.filter(id=post_id).exists():
             favorited = True
 
+    # Comments Form
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.user = user
+            comment.save()
+            return HttpResponseRedirect(reverse('postdetails', args=[post_id]))
+    else:
+        form = CommentForm()
+
     template = loader.get_template('post_detail.html')
+
     context = {
         'post': post,
         'favorited': favorited,
+        'profile': profile,
+        'form': form,
+        'comments': comments,
     }
+
     return HttpResponse(template.render(context, request))
 
 

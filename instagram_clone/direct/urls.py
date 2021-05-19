4c1e1b5a859265cd31083646e639266ad0d901a1
saharfk78 <sahar.kashany@yahoo.com>
2021-05-20 02:20:50 +0430

    sending mesaages is ok now

--- instagram_clone/direct/urls.py
+++ instagram_clone/direct/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from direct.views import inbox
+from direct.views import inbox, Directs, SendDirect
 
 # , NewPost, PostDetails, tags, like, favorite
 
 urlpatterns = [
     path('', inbox, name='inbox'),
+    path('directs/<username>', Directs, name='directs'),
+    path('send/', SendDirect, name='send_directs'),
 
 ]
--- instagram_clone/direct/views.py
+++ instagram_clone/direct/views.py
@@ -1,8 +1,8 @@
 from django.template import loader
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth.models import User
 from direct.models import Message
 
 
@@ -32,3 +32,39 @@ def inbox(request):
     template = loader.get_template('direct/direct.html')
 
     return HttpResponse(template.render(context, request))
+
+
+@login_required
+def Directs(request, username):
+    user = request.user
+    messages = Message.get_messages(user=user)
+    active_direct = username
+    directs = Message.objects.filter(user=user, recipient__username=username)
+    directs.update(is_read=True)
+    for message in messages:
+        if message['user'].username == username:
+            message['unread'] = 0
+
+    context = {
+        'directs': directs,
+        'messages': messages,
+        'active_direct': active_direct,
+    }
+
+    template = loader.get_template('direct/direct.html')
+
+    return HttpResponse(template.render(context, request))
+
+
+@login_required
+def SendDirect(request):
+    from_user = request.user
+    to_user_username = request.POST.get('to_user')
+    body = request.POST.get('body')
+
+    if request.method == 'POST':
+        to_user = User.objects.get(username=to_user_username)
+        Message.send_message(from_user, to_user, body)
+        return redirect('inbox')
+    else:
+        HttpResponseBadRequest()
